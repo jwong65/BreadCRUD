5805e9e84bae27bd34b45665ff9a5df9d5ea33b4
jwong65 <jason.wong6507@gmail.com>
2022-12-15 18:46:54 -0500

    Moved files into directory and added configuration for environmental variables

---
+++ breads/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+//Requried configuration for the environment variable
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+//console.log(process.env.PORT) They are the same.
+
+
