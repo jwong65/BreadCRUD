5955b7ed3e415d009897db61859c627798abbdce
jwong65 <jason.wong6507@gmail.com>
2022-12-15 19:51:12 -0500

    Added directories for views

--- breads/controllers/breads_controllers.js
+++ breads/controllers/breads_controllers.js
@@ -12,6 +12,7 @@ breads.get('/', (req, res)=>{
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
+    //res.send(`<img src='Bread[req.params.arrayIndex].image'></img>`)
     let breadimg = Bread[req.params.arrayIndex].image
     //res.send(Bread[req.params.arrayIndex])
     res.send(`<img src=${breadimg}></img>`)
--- breads/server.js
+++ breads/server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT
 console.log(PORT)
 //console.log(process.env.PORT) They are the same.
 
+//Middleware
+app.set('views', _dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //Routes for the pages
 app.get('/', (req, res)=>{
     res.send("First page about bread")
