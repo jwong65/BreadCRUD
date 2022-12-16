ed4545784dd23ea0a892150834f07a289acdf87a
jwong65 <jason.wong6507@gmail.com>
2022-12-15 19:06:48 -0500

    Added another models directory and added the connection to Bread

--- breads/controllers/breads_controllers.js
+++ breads/controllers/breads_controllers.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const breads = express.Router()
 
+//Connect to the model directory
+const Bread = require('../models/bread.js')
+
 
 breads.get('/', (req, res)=>{
-    res.send('This is the index at /breads')
+    //res.send('This is the index at /breads')
+    res.send(Bread)
 })
 module.exports = breads
 
