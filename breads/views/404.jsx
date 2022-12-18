09692bb6a47407117ada4a2d093d2b9b12e76d09
jwong65 <jason.wong6507@gmail.com>
2022-12-18 10:26:30 -0500

    Added the view for 404

--- breads/views/404.jsx
+++ breads/views/404.jsx
@@ -1,2 +1,15 @@
 const React = require('react')
 const Default = require('./layouts/default')
+
+
+function page404(){
+    return(
+        <Default>
+            <h2>This is a 404 error</h2>
+            <li><a href='/breads'>Return to the homepage</a></li>
+        </Default>
+    )
+
+}
+
+module.exports = page404
