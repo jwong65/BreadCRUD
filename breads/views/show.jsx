6e6d0c2b333f201283ed70aa71573b7013133d2a
jwong65 <jason.wong6507@gmail.com>
2022-12-18 09:57:38 -0500

    Added operator to the jsx file

--- breads/views/show.jsx
+++ breads/views/show.jsx
@@ -6,7 +6,29 @@ function Show ({bread}) {
     return (
       <Default>
         <h2>Show Page</h2>
+        <h3>{bread.name}</h3>
+
+        <p>
+            and it
+            {
+                bread.hasGluten
+                ? <span> does</span>
+                : <span> does NOT</span>
+            }
+
+            have gluten.
+        </p>
+
+
+
+        <img src={bread.image} alt={bread.name} />
+
+        {/* Link back to homepage */}
+        <li>
+            <a href="/breads">Go Home</a>
+        </li>
       </Default>
+
     )
 }
 
