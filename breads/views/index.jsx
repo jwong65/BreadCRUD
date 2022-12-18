8fc382229d89d588fabc35160cbfe7bfe0fc2488
jwong65 <jason.wong6507@gmail.com>
2022-12-18 09:48:43 -0500

    Added show.jsx to views directory

--- breads/views/index.jsx
+++ breads/views/index.jsx
@@ -3,7 +3,7 @@ const Default = require('./layouts/default')
 
 function Index ({breads, title}) {
     return (
-      <Default title={ title}>
+      <Default title={title}>
         <h2>Index Page</h2>
         {/* <p>I have {breads[0].name} bread.</p> */}
         <ul>
