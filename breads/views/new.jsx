0bea38d4dcdc3cf36b4ac2830e008efa593132d1
jwong65 <jason.wong6507@gmail.com>
2022-12-18 11:02:47 -0500

    Form has been added to the new.jsx

--- breads/views/new.jsx
+++ breads/views/new.jsx
@@ -3,11 +3,32 @@ const Default = require('./layouts/Default')
 
 function newPage () {
     return (
-      <Default>
-        <h2>Add a new bread</h2>
-      </Default>
-    )
+        <Default>
+          <h2>Add a new bread</h2>
+          <form >
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+            />
+            <label htmlFor="image">Image</label>
+            <input
+              type="text"
+              name="image"
+              id="image"/>
+            <label htmlFor="hasGluten">Has Gluten?</label>
+            <input
+              type="checkbox"
+              name="hasGluten"
+              id="hasGluten"
+              defaultChecked
+            />
+            <br />
+            <input type="submit"/>
+          </form>
+        </Default>
+      )
 }
 
 module.exports = newPage
-
