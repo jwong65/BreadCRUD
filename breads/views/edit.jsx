9cdf4522dfb25dac4cbaf2436444eaa9332b4b5d
jwong65 <jason.wong6507@gmail.com>
2022-12-20 18:59:32 -0500

    Added edit.jsx and skeleton with react

---
+++ breads/views/edit.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Default = require('./layouts/default')
+
+function edit(){
+    return(
+        <Default>
+            <h2> Edit this bread please</h2>
+        </Default>
+    )
+}
+
+module.exports=edit
