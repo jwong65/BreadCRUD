685e88b49fc935a74324408fc1846b24a94b0ad9
jwong65 <jason.wong6507@gmail.com>
2022-12-20 19:03:51 -0500

    Added the form to the edit.jsx

--- breads/views/edit.jsx
+++ breads/views/edit.jsx
@@ -5,6 +5,24 @@ function edit(){
     return(
         <Default>
             <h2> Edit this bread please</h2>
+            <form>
+                {/* Name is required */}
+                <label htmlFor='name'>Name</label>
+                <input type="text" name = "name" id='name' required/>
+
+                <label htmlFor='image'>IMAGE</label>
+                <input type="text" name = "image" id='image'/>
+
+{/* This form has a checkbox for just yes/no */}
+                <label htmlFor='hasGluten'>Does it have Gluten?</label>
+                <input type="checkbox" name = "hasGluten" id='hasGluten' defaultChecked/>
+
+
+
+
+                <input type = "submit"></input>
+            </form>
+
         </Default>
     )
 }
