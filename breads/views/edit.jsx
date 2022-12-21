6fd50d3b59ff386478f7241531d17ee80b8d6d89
jwong65 <jason.wong6507@gmail.com>
2022-12-20 19:08:15 -0500

    Removed default checked for hasGluten and added defaultValue to all forms

--- breads/views/edit.jsx
+++ breads/views/edit.jsx
@@ -1,21 +1,21 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function edit(){
+function edit({bread, index}){
     return(
         <Default>
             <h2> Edit this bread please</h2>
             <form>
                 {/* Name is required */}
                 <label htmlFor='name'>Name</label>
-                <input type="text" name = "name" id='name' required/>
+                <input type="text" name = "name" id='name' required defaultValue={bread.name}/>
 
                 <label htmlFor='image'>IMAGE</label>
-                <input type="text" name = "image" id='image'/>
+                <input type="text" name = "image" id='image' defaultValue={bread.image}/>
 
 {/* This form has a checkbox for just yes/no */}
                 <label htmlFor='hasGluten'>Does it have Gluten?</label>
-                <input type="checkbox" name = "hasGluten" id='hasGluten' defaultChecked/>
+                <input type="checkbox" name = "hasGluten" id='hasGluten' defaultValue={bread.hasGluten}/>
 {/* Break because button is being overlapped by checkbox. */}
                 <br></br>
 
