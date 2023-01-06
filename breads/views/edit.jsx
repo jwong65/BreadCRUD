255cd713cc01b8c4c6ef678c9e5245e7fc808c4e
jwong65 <jason.wong6507@gmail.com>
2023-01-05 20:06:24 -0500

    Added the select field for the baker in edit.jsx

--- breads/views/edit.jsx
+++ breads/views/edit.jsx
@@ -13,6 +13,16 @@ function edit({bread}){
                 <label htmlFor='image'>IMAGE</label>
                 <input type="text" name = "image" id='image' defaultValue={bread.image}/>
 
+                {/* Need to have field to update bakers */}
+                <label htmlFor='baker'>Baker who baked it</label>
+                <select name='baker' id='baker' defaultValue={bread.baker}>
+                    <option value='Rachel'>Rachel</option>
+                    <option value='Monica'>Monica</option>
+                    <option value='Joey'>Joey</option>
+                    <option value='Chandler'>Chandler</option>
+                    <option value='Ross'>Ross</option>
+                    <option value='Phoebe'>Phoebe</option>
+                </select>
 {/* This form has a checkbox for just yes/no */}
                 <label htmlFor='hasGluten'>Does it have Gluten?</label>
                 <input type="checkbox" name = "hasGluten" id='hasGluten' defaultChecked={bread.hasGluten}/>
