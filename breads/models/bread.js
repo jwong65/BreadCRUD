3829e254f6d874cac3c258a2e3721d0d4bdedd80
jwong65 <jason.wong6507@gmail.com>
2023-01-08 11:24:45 -0500

    Changed the baker.startDate so it would be in bread.js

--- breads/models/bread.js
+++ breads/models/bread.js
@@ -17,7 +17,7 @@ const breadSchema =  new Schema({
 //Instance
 breadSchema.methods.bakedBy= function(){
   //Instead of bread, use this.name
-  return `${this.name} was baked by ${this.baker.name}`
+  return `${this.name} was baked by ${this.baker.name}, who has been employed since ${this.baker.startDate}`
 }
 
 // Will work on later possibly
