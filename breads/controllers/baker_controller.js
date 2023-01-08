9254cd842c020f8f2ce1d947b984e6b8072f897d
jwong65 <jason.wong6507@gmail.com>
2023-01-08 10:10:51 -0500

    Added the require to the models directory for baker_controller

--- breads/controllers/baker_controller.js
+++ breads/controllers/baker_controller.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const baker = express.Router()
 
+const bakerModel = require('../models/baker')
+const bakerSeedData = require('../models/baker_Seed')
 
 module.exports = baker
