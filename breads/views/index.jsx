374fdeb220f8b8945eb9c0f3e973fc6869d191ee
jwong65 <jason.wong6507@gmail.com>
2023-01-08 13:11:11 -0500

    Added the bakers section to the breads index similar to the bread listing

--- breads/views/index.jsx
+++ breads/views/index.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index ({breads, title}) {
+function Index ({breads, bakers, title}) {
     return (
       <Default title={title}>
         <h2>Index Page</h2>
         {/* <p>I have {breads[0].name} bread.</p> */}
+        <h3>Bread</h3>
         <ul>
             {
                     breads.map((bread, index)=> {
@@ -23,6 +24,18 @@ function Index ({breads, title}) {
                     })
 
             }
+        </ul>
+        <h3>Bakers</h3>
+        <ul>
+          {
+            bakers.map((baker)=>{
+              return(
+                <li key={baker.id}>
+                  <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+                </li>
+              )
+            })
+          }
         </ul>
         <div className="newButton">
           <a href="/breads/new"><button>Add a new bread</button></a>
