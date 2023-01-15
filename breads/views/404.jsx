58240b50bc8de7806e525531b6089dc91ccd12ed
jwong65 <jason.wong6507@gmail.com>
2023-01-15 12:26:27 -0500

    Testing a change in the view

--- breads/views/404.jsx
+++ breads/views/404.jsx
@@ -5,7 +5,7 @@ const Default = require('./layouts/default')
 function page404(){
     return(
         <Default>
-            <h2>This is a 404 error</h2>
+            <h2>This is a 404 error. Please Test</h2>
             <li><a href='/breads'>Return to the homepage</a></li>
         </Default>
     )
