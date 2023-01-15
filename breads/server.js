c200774dbf8c153d4d18829fc3f289e4296efaae
jwong65 <jason.wong6507@gmail.com>
2023-01-15 12:02:10 -0500

    Changed the path for build

--- breads/server.js
+++ breads/server.js
@@ -47,7 +47,7 @@ app.get('*', (req, res)=>{
 })
   
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+    //() => { console.log('connected to mongo: ', process.env.MONGO_URI) }
   )
 
 app.listen(PORT)
